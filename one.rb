62b7c5429e294c49378fe7c69f36724763663613
RamonaEJSpence <ramonaejspence@gmail.com>
2020-09-07 18:19:53 -0400

    changed regexp comparison operator in while loop. changed how to check index is empty or not empty. used for loop to handle multiple indices

--- one.rb
+++ one.rb
@@ -8,60 +8,44 @@ words = [
     'hibernation'
 ]
 
-chances = 15
-
-
-
 puts 'Hi, this is my first Ruby program!'
 sleep(1)
 puts 'Why don\'t we play a quick game of Hangman?'
 sleep(1)
 puts 'The word will be chosen randomly from a list and you get 15 chances to guess.'
 
-index = rand(7)
-word = words[index] 
+chances = 15
+
+word = words[rand(words.length())] 
 result = '_' * word.length()
 result.split('')
 puts result
+
 chars = word.split('')
-p chars
 
-# puts 'Your first guess..'
-# guess1 = gets.chomp()
-
-repeat = 0
-index = 0
+index = []
 puts "Your first guess..."
-while chances > 0
 
-guess1 = gets.chomp()
-    for i in chars
-        if i == guess1
-            index = chars.index(i)
-            repeat += 1
-            chances -= 1
-
-            puts "Nice!"
-            result[index] = guess1      
-            puts result
-        else
-            puts "Sorry! But noooope. Guess again though!"
-            chances -= 1
-        end
+while chances > 0 && result =~ /_/
+    guess1 = gets.chomp()
+    index = chars.each_index.select { |i| chars[i] == guess1 }
+    chances -= 1
+    if index != []
+        puts 'Got one!'
+    elsif index == []
+        puts 'Nope! Guess again..'
     end
+
+    for i in index
+        result[i] = guess1
+    end 
+    p result
+
 end
-"""
-working so far needs to handle guesses that show up more than once
-"""
 
+if result =~ /_/ && chances == 0
+    puts "you ran out of chances"
+elsif chances > 0 && result !=~ /_/
+    puts 'you win'
+end
 
-# if repeat == 1
-#     puts "Nice! You guessed one!"
-#     result[index] = guess1
-# else
-#     puts "Nice! There are #{repeat}!"
-#     result[index] = guess1
-# end
-
-result[index] = guess1      
-puts result
